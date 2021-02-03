d3e95780ff8577b2b599a4002047dd72a1f035b1
Fong Kin Shing <ksfong.2017@sis.smu.edu.sg>
2021-02-03 18:31:15 +0800

    Enable Subcomment insertion

--- salt-rice/salt-rice-be/src/service/comment-service.js
+++ salt-rice/salt-rice-be/src/service/comment-service.js
@@ -3,102 +3,111 @@
 //delete
 //flag
 
-import {dbConnPool} from '../repo/db-client'
-import {updateScoreInDB} from '../repo/user-repo';
+import { dbConnPool } from '../repo/db-client';
+import { updateScoreInDB } from '../repo/user-repo';
 
 let createComment = async (input_json) => {
-    let conn;
-    try {
+	let conn;
+	try {
+		conn = await dbConnPool.getConnection();
 
-      conn = await dbConnPool.getConnection();
-      const resp = await conn.query("INSERT into comment (body, postID, authorID, isAnonymous) VALUES ( ?, ?, ?, ?)", [ input_json['body'], input_json['postID'], input_json['authorID'], input_json['isAnonymous']]);
-      console.log(resp);
+		if (input_json.hasOwnProperty('parentCommentID')) {
+			const resp = await conn.query(
+				'INSERT into comment (body, postID, authorID, isAnonymous, parentCommentID) VALUES ( ?, ?, ?, ?, ?)',
+				[input_json['body'], input_json['postID'], input_json['authorID'], input_json['isAnonymous'], input_json['parentCommentID']]
+			);
+			console.log(resp);
 
-      await updateScoreInDB(input_json['authorID'], 5)
+			await updateScoreInDB(input_json['authorID'], 5);
 
-      return resp;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) conn.release(); //release to pool
-    }
-  }
+			return resp;
+		} else {
+			const resp = await conn.query(
+				'INSERT into comment (body, postID, authorID, isAnonymous) VALUES ( ?, ?, ?, ?)',
+				[input_json['body'], input_json['postID'], input_json['authorID'], input_json['isAnonymous']]
+			);
+			console.log(resp);
 
-  let searchComments = async (commentID) => {
-    let conn;
-    try {
+			await updateScoreInDB(input_json['authorID'], 5);
 
-      conn = await dbConnPool.getConnection();
-      const resp = await conn.query("select * from comment where commentID="+commentID);
-      console.log(resp);
-      return resp;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) conn.release(); //release to pool
-    }
-  }
+			return resp;
+		}
+	} catch (err) {
+		throw err;
+	} finally {
+		if (conn) conn.release(); //release to pool
+	}
+};
 
-  let flagComment = async (commentID) => {
-    let conn;
-    try {
+let searchComments = async (commentID) => {
+	let conn;
+	try {
+		conn = await dbConnPool.getConnection();
+		const resp = await conn.query('select * from comment where commentID=' + commentID);
+		console.log(resp);
+		return resp;
+	} catch (err) {
+		throw err;
+	} finally {
+		if (conn) conn.release(); //release to pool
+	}
+};
 
-      conn = await dbConnPool.getConnection();
-      const resp = await conn.query("UPDATE comment set isFlagged=1 where commentID="+commentID);
-      console.log(resp);
-      return resp;
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) conn.release(); //release to pool
-    }
-  }
+let flagComment = async (commentID) => {
+	let conn;
+	try {
+		conn = await dbConnPool.getConnection();
+		const resp = await conn.query('UPDATE comment set isFlagged=1 where commentID=' + commentID);
+		console.log(resp);
+		return resp;
+	} catch (err) {
+		throw err;
+	} finally {
+		if (conn) conn.release(); //release to pool
+	}
+};
 
-  let deleteComment = async (commentID) => {
-    let conn;
-    try {
-      conn = await dbConnPool.getConnection();
-      const rows = await conn.query("DELETE from comment where commentID="+commentID);
-      return rows
-    } catch (err) {
-      throw err;
-    } finally {
-      if (conn) conn.release(); //release to pool
-    }
-  }
+let deleteComment = async (commentID) => {
+	let conn;
+	try {
+		conn = await dbConnPool.getConnection();
+		const rows = await conn.query('DELETE from comment where commentID=' + commentID);
+		return rows;
+	} catch (err) {
+		throw err;
+	} finally {
+		if (conn) conn.release(); //release to pool
+	}
+};
 
 let upvoteComment = async (commentID) => {
-  let conn;
-  try {
-    conn = await dbConnPool.getConnection();
-    const resp = await conn.query("UPDATE comment SET upVote = upVote + 1 WHERE commentID = "+commentID);
+	let conn;
+	try {
+		conn = await dbConnPool.getConnection();
+		const resp = await conn.query('UPDATE comment SET upVote = upVote + 1 WHERE commentID = ' + commentID);
 
-    let commentObj = searchComments(req.params.commentId);
-    await updateScoreInDB(commentObj.authorID, 1);
+		let commentObj = searchComments(req.params.commentId);
+		await updateScoreInDB(commentObj.authorID, 1);
 
-
-    return resp;
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) conn.release(); //release to pool
-  }
+		return resp;
+	} catch (err) {
+		throw err;
+	} finally {
+		if (conn) conn.release(); //release to pool
+	}
 };
 
 let downvoteComment = async (commentID) => {
-  let conn;
-  try {
-    conn = await dbConnPool.getConnection();
-    const resp = await conn.query("UPDATE comment SET downVote = downVote + 1 WHERE commentID = "+commentID);
-    return resp;
-  } catch (err) {
-    throw err;
-  } finally {
-    if (conn) conn.release(); //release to pool
-  }
+	let conn;
+	try {
+		conn = await dbConnPool.getConnection();
+		const resp = await conn.query('UPDATE comment SET downVote = downVote + 1 WHERE commentID = ' + commentID);
+		return resp;
+	} catch (err) {
+		throw err;
+	} finally {
+		if (conn) conn.release(); //release to pool
+	}
 };
 
-export {
-  createComment, searchComments, flagComment, deleteComment,
-  upvoteComment, downvoteComment
-}
+export { createComment, searchComments, flagComment, deleteComment, upvoteComment, downvoteComment };
