5dd49388440567bc28d471bac4c5c8e9298d2781
Fong Kin Shing <ksfong.2017@sis.smu.edu.sg>
2021-04-18 22:25:51 +0800

    Add Login Button

--- salt-rice/salt-rice-fe/src/components/Header.vue
+++ salt-rice/salt-rice-fe/src/components/Header.vue
@@ -1,319 +1,334 @@
 <template>
-  <header>
-    <router-link to="/" tag="a">
-      <img src="/images/saltrice.svg" alt="SaltRice logo" class="logo" />
-    </router-link>
-    
-    <nav v-if="isLoggedIn">
-      <div class="icon-login">
-        <a
-          @click="showEditProfile = !showEditProfile"
-          href="#"
-          class="hide-desktop"
-        >
-          <img
-            src="@/assets/images/iconfinder_1_avatar_2754574.svg"
-            alt="toogle user-icon"
-            class="usericon-menu"
-            id="user-icon-mobile"
-          />
-        </a>
-        <a href="#" class="hide-desktop">
-          <img
-            src="@/assets/images/ham.png"
-            alt="toogle menu"
-            class="menu"
-            id="menu"
-            @click="showMenu = !showMenu"
-          />
-        </a>
-      </div>
-      <ul v-bind:class="showMenu ? '' : 'show-desktop'" id="nav">
-        <li
-          @click="showMenu = !showMenu"
-          id="exit"
-          class="exit-btn hide-desktop"
-        >
-          <img src="@/assets/images/exit.svg" alt="" />
-        </li>
-        <li>
-          <router-link to="/" exact active-class="bold">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/post" exact active-class="bold"
-            >Create post</router-link
-          >
-        </li>
-        <li>
-          <a
-            @click="showEditProfile = !showEditProfile"
-            id="editprofile"
-            class="hide-mobile"
-            ><span class="inline-flex"
-              ><img
-                src="@/assets/images/iconfinder_1_avatar_2754574.svg"
-                alt="toogle user-icon"
-                class="usericon-menu"
-                id="user-icon-desktop"
-              /><span class="usericon-label">{{username}}</span></span
-            ></a
-          >
-        </li>
-      </ul>
-    </nav>
-    <div
-      v-bind:class="showEditProfile ? 'show' : ''"
-      class="edit-profile hide"
-      id="editprofile-screen"
-      v-if="isLoggedIn"
-    >
-      <div class="head">
-        <img
-          src="@/assets/images/iconfinder_1_avatar_2754574.svg"
-          alt="toogle user-icon"
-          class="usericon-edit"
-          id="user-icon-edit"
-        />
+	<header>
+		<router-link to="/" tag="a">
+			<img src="/images/saltrice.svg" alt="SaltRice logo" class="logo" />
+		</router-link>
 
-        <a href="#">
-          <img
-            src="@/assets/images/edit.png"
-            alt="toogle icon-pen"
-            class="icon-pen"
-            id="icon-pen-picture"
-          />
-        </a>
-      </div>
-      <div class="border"></div>
-      <div class="body">
-        <div class="nickname">
-          <p>{{rank}}</p>
-          <!-- <a href="#"
+		<nav v-if="isLoggedIn">
+			<div class="icon-login">
+				<a @click="showEditProfile = !showEditProfile" href="#" class="hide-desktop">
+					<img
+						src="@/assets/images/iconfinder_1_avatar_2754574.svg"
+						alt="toogle user-icon"
+						class="usericon-menu"
+						id="user-icon-mobile"
+					/>
+				</a>
+				<a href="#" class="hide-desktop">
+					<img
+						src="@/assets/images/ham.png"
+						alt="toogle menu"
+						class="menu"
+						id="menu"
+						@click="showMenu = !showMenu"
+					/>
+				</a>
+			</div>
+			<ul v-bind:class="showMenu ? '' : 'show-desktop'" id="nav">
+				<li @click="showMenu = !showMenu" id="exit" class="exit-btn hide-desktop">
+					<img src="@/assets/images/exit.svg" alt="" />
+				</li>
+				<li>
+					<router-link to="/" exact active-class="bold">Home</router-link>
+				</li>
+				<li>
+					<router-link to="/post" exact active-class="bold">Create post</router-link>
+				</li>
+				<li>
+					<a @click="showEditProfile = !showEditProfile" id="editprofile" class="hide-mobile"
+						><span class="inline-flex"
+							><img
+								src="@/assets/images/iconfinder_1_avatar_2754574.svg"
+								alt="toogle user-icon"
+								class="usericon-menu"
+								id="user-icon-desktop"
+							/><span class="usericon-label">{{ username }}</span></span
+						></a
+					>
+				</li>
+			</ul>
+		</nav>
+		<nav v-if="!isLoggedIn">
+			<div style="cursor:pointer">
+				<a class="cte bold" href="/login">Login</a>
+			</div>
+		</nav>
+		<div
+			v-bind:class="showEditProfile ? 'show' : ''"
+			class="edit-profile hide"
+			id="editprofile-screen"
+			v-if="isLoggedIn"
+		>
+			<div class="head">
+				<img
+					src="@/assets/images/iconfinder_1_avatar_2754574.svg"
+					alt="toogle user-icon"
+					class="usericon-edit"
+					id="user-icon-edit"
+				/>
+
+				<a href="#">
+					<img src="@/assets/images/edit.png" alt="toogle icon-pen" class="icon-pen" id="icon-pen-picture" />
+				</a>
+			</div>
+			<div class="border"></div>
+			<div class="body">
+				<div class="nickname">
+					<p>{{ rank }}</p>
+					<!-- <a href="#"
             ><img
               src="@/assets/images/edit.png"
               alt="toogle icon-pen"
               class="icon-pen"
               id="icon-pen-nickname"
           /></a> -->
-        </div>
-        
-        <!-- <div class="margin-top">
+				</div>
+
+				<!-- <div class="margin-top">
           <span>25 </span><span>Posts</span>
         </div>
         <div class="margin-top"><span>25 </span><span>Comments</span></div> -->
-        <div class="margin-top logout" @click="logoutComp"><span>logout</span></div>
-      </div>
-    </div>
-  </header>
+				<div class="margin-top logout" @click="logoutComp"><span>logout</span></div>
+			</div>
+		</div>
+	</header>
 </template>
 
 <script>
-import {mapActions, mapGetters, mapState} from 'vuex';
+import { mapActions, mapGetters, mapState } from 'vuex';
 
 export default {
-  name: "Header",
-  data() {
-    return {
-      showMenu: false,
-      showEditProfile: false,
-      /* username: '', */
-      /* username: store.getters.getUsername, */
-    };
-  },
-  methods: {
-    ...mapActions(['logout']),
-    async logoutComp(){
-      let response = await this.logout()
-      if (response.status == 200) {
-        await this.$router.push('/login');
-      }
-    }
-
-  },
-  computed: {
-    ...mapState({
-      /* isLoggedIn : 'isLoggedIn', */
-      username: state => state.auth.username,
-      rank: state => state.auth.principal.rank,
-    }),
-    ...mapGetters(['isLoggedIn']),
-  }
+	name: 'Header',
+	data() {
+		return {
+			showMenu: false,
+			showEditProfile: false,
+			/* username: '', */
+			/* username: store.getters.getUsername, */
+		};
+	},
+	methods: {
+		...mapActions(['logout']),
+		async logoutComp() {
+			let response = await this.logout();
+			if (response.status == 200) {
+				await this.$router.push('/login');
+			}
+		},
+	},
+	computed: {
+		...mapState({
+			/* isLoggedIn : 'isLoggedIn', */
+			username: (state) => state.auth.username,
+			rank: (state) => state.auth.principal.rank,
+		}),
+		...mapGetters(['isLoggedIn']),
+	},
 };
 </script>
 <style lang="scss">
 header {
-  display: flex;
-  justify-content: space-between;
-  padding: side-space(mobile);
-  z-index: 4;
-  position: sticky;
-  top: 0;
-  background-color: color(beidge);
+	display: flex;
+	justify-content: space-between;
+	padding: side-space(mobile);
+	z-index: 4;
+	position: sticky;
+	top: 0;
+	background-color: color(beidge);
 
-  @include layout(pc) {
-    padding: 0.8em side-space(pc) 0 side-space(pc);
-  }
+	@include layout(pc) {
+		padding: 0.8em side-space(pc) 0 side-space(pc);
+	}
+	.cte {
+		
+		padding: 0.8em 0.8em 0.8em 0.8em;
+		margin-top: 20%;
+		
+		background: #ffc529;
+		text-decoration: none;
+		color: black;
+		font-size: 12pt;
+		font-weight: bold;
+		width: 149px;
+		text-align: right;
 
-  .icon-login {
-    display: flex;
+		@include layout(tablet) {
+			right: side-space(mobile);
+			padding: 0.9em 0.9em 0.9em 0.9em;
+			font-size: calc(#{$font-size} + 1pt);
+			margin-top: 12%;
+			width: 167px;
+		}
 
-    a {
-      margin-left: 1em;
-    }
-  }
+		@include layout(pc) {
+			right: side-space(pc)-1.2em;
+			padding: 0.7em 0.9em 0.7em 2.2em;
+			font-size: calc(#{$font-size} + 4pt);
+			margin-top: 15%;
+			width: 216px;
+		}
+	}
 
-  .usericon-label {
-    padding: 0em 0 0 0.5em;
-  }
+	.icon-login {
+		display: flex;
 
-  .logout {
-    cursor: pointer;
-    font-weight: bold;
-  }
+		a {
+			margin-left: 1em;
+		}
+	}
 
-  .inline-flex {
-    display: inline-flex;
-  }
+	.usericon-label {
+		padding: 0em 0 0 0.5em;
+	}
 
-  .logo {
-    width: 120px;
-    @include layout(pc) {
-      width: 150px;
-      padding-top: .4em;
-    }
-  }
+	.logout {
+		cursor: pointer;
+		font-weight: bold;
+	}
 
-  .menu {
-    width: 25px;
-    margin-top: 30%;
-  }
+	.inline-flex {
+		display: inline-flex;
+	}
 
-  .usericon-menu {
-    width: 25px;
-    margin-top: 15%;
+	.logo {
+		width: 120px;
+		@include layout(pc) {
+			width: 150px;
+			padding-top: 0.4em;
+		}
+	}
 
-    @include layout(pc) {
-      margin-top: -0.1em;
-    }
-  }
+	.menu {
+		width: 25px;
+		margin-top: 30%;
+	}
 
-  nav {
-    ul {
-      position: fixed;
-      width: 60%;
-      top: 0;
-      right: 0;
-      text-align: left;
-      background: color(gray-transparent);
-      height: 100%;
-      z-index: 7;
-      padding-top: 3em;
+	.usericon-menu {
+		width: 25px;
+		margin-top: 15%;
 
-      @include layout(tablet) {
-        width: 40%;
-      }
+		@include layout(pc) {
+			margin-top: -0.1em;
+		}
+	}
 
-      @include layout(pc) {
-        position: inherit;
-        width: auto;
-        background: none;
-        height: auto;
-        display: flex;
-        padding-top: 0;
-      }
+	nav {
+		ul {
+			position: fixed;
+			width: 60%;
+			top: 0;
+			right: 0;
+			text-align: left;
+			background: color(gray-transparent);
+			height: 100%;
+			z-index: 7;
+			padding-top: 3em;
 
-      li {
-        cursor: pointer;
-        @include layout(pc) {
-          float: left;
-          padding-right: 2em;
-        }
-      }
+			@include layout(tablet) {
+				width: 40%;
+			}
 
-      li a {
-        color: color(white);
-        text-decoration: none;
-        display: block;
-        width: 100%;
-        padding: 1em 2em;
-        background: color(gray-transparent2);
+			@include layout(pc) {
+				position: inherit;
+				width: auto;
+				background: none;
+				height: auto;
+				display: flex;
+				padding-top: 0;
+			}
 
-        @include layout(pc) {
-          color: color(black);
-          background-color: inherit;
-          text-align: right;
-          padding: 1em 2em;
-        }
+			li {
+				cursor: pointer;
+				@include layout(pc) {
+					float: left;
+					padding-right: 2em;
+				}
+			}
 
-        &:hover {
-          background-color: color(gray-transparent3);
+			li a {
+				color: color(white);
+				text-decoration: none;
+				display: block;
+				width: 100%;
+				padding: 1em 2em;
+				background: color(gray-transparent2);
 
-          @include layout(pc) {
-            background-color: inherit;
-          }
-        }
-      }
+				@include layout(pc) {
+					color: color(black);
+					background-color: inherit;
+					text-align: right;
+					padding: 1em 2em;
+				}
 
-      li.exit-btn {
-        margin-bottom: 1em;
-        margin-top: -1.3em;
-        text-align: right;
-        padding: 0 1.4em;
+				&:hover {
+					background-color: color(gray-transparent3);
 
-        img {
-          width: 15px;
-          cursor: pointer;
-        }
-      }
-    }
-  }
+					@include layout(pc) {
+						background-color: inherit;
+					}
+				}
+			}
 
-  .edit-profile {
-    position: absolute;
-    background-color: color(white);
-    width: 155px;
-    right: margin-top-card-element-responsive(mobile);
-    top: 73px;
-    border: 1px solid color(black);
-    padding: 2em;
-    text-align: center;
+			li.exit-btn {
+				margin-bottom: 1em;
+				margin-top: -1.3em;
+				text-align: right;
+				padding: 0 1.4em;
 
-    @include layout(tablet) {
-      right: margin-top-card-element-responsive(tablet);
-      width: 198px;
-    }
+				img {
+					width: 15px;
+					cursor: pointer;
+				}
+			}
+		}
+	}
 
-    @include layout(pc) {
-      right: 5em;
-      top: 69px;
-    }
+	.edit-profile {
+		position: absolute;
+		background-color: color(white);
+		width: 155px;
+		right: margin-top-card-element-responsive(mobile);
+		top: 73px;
+		border: 1px solid color(black);
+		padding: 2em;
+		text-align: center;
 
-    .head {
-      display: inline-block;
-      margin-bottom: 1em;
+		@include layout(tablet) {
+			right: margin-top-card-element-responsive(tablet);
+			width: 198px;
+		}
 
-      #icon-pen-picture {
-        width: 25px;
-        position: absolute;
-        top: 4em;
-        right: 4em;
-      }
-    }
+		@include layout(pc) {
+			right: 5em;
+			top: 69px;
+		}
 
-    .body {
-      .nickname {
-        display: inline-flex;
+		.head {
+			display: inline-block;
+			margin-bottom: 1em;
 
-        #icon-pen-nickname {
-          width: 25px;
-          height: 22px;
-          margin: 1em 0.5em;
-        }
-      }
-      #rating_value {
-        margin-left: 0.5em;
-      }
-    }
-  }
+			#icon-pen-picture {
+				width: 25px;
+				position: absolute;
+				top: 4em;
+				right: 4em;
+			}
+		}
+
+		.body {
+			.nickname {
+				display: inline-flex;
+
+				#icon-pen-nickname {
+					width: 25px;
+					height: 22px;
+					margin: 1em 0.5em;
+				}
+			}
+			#rating_value {
+				margin-left: 0.5em;
+			}
+		}
+	}
 }
 </style>
